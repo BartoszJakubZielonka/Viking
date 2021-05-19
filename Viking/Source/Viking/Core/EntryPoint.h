99672c607ad11b7adc2c2ff53635daec5842e595
Bartosz Zielonka <batziell@gmail.com>
2021-05-19 20:36:32 +0200

    Fix compilation issue

--- Viking/Source/Viking/Core/EntryPoint.h
+++ Viking/Source/Viking/Core/EntryPoint.h
@@ -9,13 +9,13 @@
 #define VIKING_ENTRYPOINT_H
 
 #ifdef VI_PLATFORM_WINDOWS
-extern Viking::Application* Viking::createApplication(ApplicationCommandLineArgs args);
+extern Viking::Application* createApplication(Viking::ApplicationCommandLineArgs args);
 
 int main(int argc, char** argv) {
     Viking::Log::init();
 
     VI_PROFILE_BEGIN_SESSION("Startup", "VikingProfile-Startup.json");
-    auto app = Viking::createApplication({ argc, argv });
+    auto app = createApplication({ argc, argv });
     VI_PROFILE_END_SESSION();
 
     VI_PROFILE_BEGIN_SESSION("Runtime", "VikingProfile-Runtime.json");
