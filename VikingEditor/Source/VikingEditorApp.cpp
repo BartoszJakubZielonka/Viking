3a809742896f3f4bff7016107a17df34ce11f4ca
Bartosz Zielonka <batziell@gmail.com>
2021-05-23 13:18:42 +0200

    Added Nvidia is default GPU

--- VikingEditor/Source/VikingEditorApp.cpp
+++ VikingEditor/Source/VikingEditorApp.cpp
@@ -3,6 +3,13 @@
 
 #include "EditorLayer.h"
 
+#ifdef VI_PLATFORM_WINDOWS
+extern "C" {
+    __declspec(dllexport) uint32_t NvOptimusEnablement = 0x00000001;
+    __declspec(dllexport) int AmdPowerXpressRequestHighPerformance = 1;
+}
+#endif
+
 class VikingEditor final: public Viking::Application
 {
 public:
